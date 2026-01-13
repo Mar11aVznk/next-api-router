c601443c910a01d01e895886f860bb1f61743fdc
Mar11aVznk <marya.voznuk@gmail.com>
2026-01-13 09:48:37 +0200

    fine

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,8 +1,74 @@
-import React from 'react'
+"use client"
+import { useEffect, useState } from "react"
 
-export default function page() {
+type Location = {
+    id: number;
+    name: string;
+    lat: number;
+    lng: number;
+
+}
+
+export default function PlacePage() {
+
+    const [locations, setLocations] = useState<Location[]>([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
+
+useEffect(() => {
+  async function fetchLocations() {
+    try {
+      const res = await fetch("/api/map")
+
+      if (!res.ok) {
+
+        throw new Error("Failed to fetch data")
+      }
+
+      const data: Location[] = await res.json()
+
+      setLocations(data)
+
+    } catch {
+
+      setError("Kunde inte hämtas data från API")
+
+    } finally {
+
+      setLoading(false)
+
+    }
+  }
+
+  fetchLocations()
+
+}, [])
+
+if (loading) {
+
+  return<p>Laddar...</p>
+
+}
+
+if (error) {
+
+  return <p>{error}</p>
+
+}
+
   return (
     <div>
+      <h1>Platser från API-route</h1>
+      <p>Denna data hämtas från <code>/api/map</code></p>
+      <ul>
+        {locations.map((location) =>(
+          <li key={location.id}>
+            <strong>{location.name}</strong>
+            <br/>
+            Lat: {location.lat}, Lng: {location.lng}
+          </li>
+        ))}
+      </ul>
        
     </div>
   )
