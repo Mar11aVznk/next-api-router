d9a6ddc017f9ea48864df0dff94a0a1343e8145b
Mar11aVznk <marya.voznuk@gmail.com>
2026-01-15 13:55:26 +0200

    post

--- app/api/map/route.ts
+++ app/api/map/route.ts
@@ -1,5 +1,4 @@
-export async function GET() {
-    return Response.json([ 
+let map = [ 
         { "id": 1, 
           "name": "Skolan",
           "lat": 59.3, 
@@ -8,5 +7,19 @@ export async function GET() {
           "name": "Biblioteket", 
           "lat": 59.4, 
           "lng": 18.05 } 
-    ])
+    ]
+export async function GET() {
+    return Response.json(map)
+}
+
+export async function POST(req: Request) {
+  const data = await req.json()
+  const newData = {
+    id: map.length + 1,
+    name: data.name,
+    lat: data.lat,
+    lng: data.lng
+  }
+ map.push(newData)
+  return Response.json({msg: "Added successful"})
 }
--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import { useEffect, useState } from "react"
 
 type Location = {
@@ -15,53 +16,72 @@ export default function PlacePage() {
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState<string | null>(null)
 
-useEffect(() => {
-  async function fetchLocations() {
-    try {
+    const [name, setName] = useState("")
+    const [lat, setLat] = useState<number | undefined>()
+    const [lng, setLng] = useState<number | undefined>()
+    const [update, setUpdate] = useState(false)
+
+
+    async function fetchMapData() {
       const res = await fetch("/api/map")
-
-      if (!res.ok) {
-
-        throw new Error("Failed to fetch data")
-      }
-
-      const data: Location[] = await res.json()
-
+      const data = await res.json()
       setLocations(data)
-
-    } catch {
-
-      setError("Kunde inte hämtas data från API")
-
-    } finally {
-
-      setLoading(false)
-
     }
+
+  useEffect(()=> {
+      fetchMapData()
+  }, [update])
+
+  useEffect(() => {
+    async function fetchLocations() {
+      try {
+          const res = await fetch("/api/map")
+          if (!res.ok) {
+          throw new Error("Failed to fetch data")
+          }
+        const data: Location[] = await res.json()
+        setLocations(data)
+      } catch {
+        setError("Kunde inte hämtas data från API")
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchLocations()
+  }, [])
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    const res = await fetch("/api/map", {
+      method: 'post',
+      body: JSON.stringify({name, lat, lng})
+    })
+
+    const data = await res.json()
+
+    setName("")
+    setLat(undefined)
+    setLng(undefined)
+
+    setUpdate(!update)
+
   }
 
-  fetchLocations()
+  if (loading) {
+    return<p className="mt-10 ml-10 text-2xl">Laddar...</p>
+  }
 
-}, [])
-
-if (loading) {
-
-  return<p>Laddar...</p>
-
-}
-
-if (error) {
-
-  return <p>{error}</p>
-
-}
+  if (error) {
+    return <p>{error}</p>
+  }
 
   return (
-    <div>
-      <h1>Platser från API-route</h1>
-      <p>Denna data hämtas från <code>/api/map</code></p>
-      <ul>
-        {locations.map((location) =>(
+
+    <div className="ml-10">
+      <h1 className="text-2xl mt-5">Platser från API-route</h1>
+      <p className=" text-gray-700 italic mt-5">Denna data hämtas från <code>/api/map</code></p>
+      <ul className="mt-5">
+        {locations.map((location) => (
           <li key={location.id}>
             <strong>{location.name}</strong>
             <br/>
@@ -69,7 +89,19 @@ if (error) {
           </li>
         ))}
       </ul>
+      
+    <form onSubmit={handleSubmit}>
+      <input className="bg-amber-50 mt-5 border border-amber-600 pl-2" onChange={(e)=>setName(e.target.value)} type="text" value={name} placeholder="Sriv en plats..." />
+        <br/>
+      <input className="bg-amber-50 mt-5 border border-amber-600 pl-2" onChange={(e) => setLat(Number(e.target.value))} value={lat ?? ""} type="number"/>
+        <br/>
+      <input className="bg-amber-50 mt-5 border border-amber-600 pl-2" onChange={(e)=> setLng(Number(e.target.value))} value={lng ?? ""} type="number"/>
+        <br/>
+      <button className="bg-amber-600 mt-5 w-20 border border-amber-900">Send</button>
+      </form>
        
     </div>
+
   )
+
 }
