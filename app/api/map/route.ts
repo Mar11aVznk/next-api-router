deced110dd373787ebb0011cb99a7ecc12d69e78
Mar11aVznk <marya.voznuk@gmail.com>
2026-01-21 21:36:13 +0200

    put

--- app/api/map/route.ts
+++ app/api/map/route.ts
@@ -1,4 +1,4 @@
-let map = [ 
+let mapArr = [ 
         { "id": 1, 
           "name": "Skolan",
           "lat": 59.3, 
@@ -9,17 +9,54 @@ let map = [
           "lng": 18.05 } 
     ]
 export async function GET() {
-    return Response.json(map)
+    return Response.json(mapArr)
 }
 
 export async function POST(req: Request) {
   const data = await req.json()
+
+  const maxId = mapArr.length > 0
+    ? Math.max(...mapArr.map(item => item.id))
+    : 0
   const newData = {
-    id: map.length + 1,
+    id: maxId + 1,
     name: data.name,
     lat: data.lat,
     lng: data.lng
   }
- map.push(newData)
-  return Response.json({msg: "Added successful"})
+ mapArr.push(newData)
+  return Response.json(newData)
+}
+
+export async function DELETE(req:Request) {
+  const {id} = await req.json()
+
+  mapArr= mapArr.filter(item => item.id !== id)
+  return Response.json({})
+}
+
+export async function PUT(req: Request) {
+  const body = await req.json()
+  const {id, name, lat, lng} = body
+
+  if (!id) {
+    return Response.json({error: "Ingen id"}, {status: 400})
+  }
+
+  const index = mapArr.findIndex(item => item.id === Number(id))
+
+  if (index === -1) {
+      return Response.json({error: "Lokation finns inte"}, {status: 400})
+  }
+
+  mapArr[index] = {
+    ...mapArr[index],
+    name,
+    lat,
+    lng
+  }
+
+
+  return Response.json(mapArr[index])
+
 }
--- app/map/page.tsx
+++ app/map/page.tsx
@@ -14,46 +14,50 @@ export default function PlacePage() {
 
     const [locations, setLocations] = useState<Location[]>([])
     const [loading, setLoading] = useState(true)
-    const [error, setError] = useState<string | null>(null)
 
     const [name, setName] = useState("")
     const [lat, setLat] = useState<number | undefined>()
     const [lng, setLng] = useState<number | undefined>()
     const [update, setUpdate] = useState(false)
 
+    const [updateId, setUpdateId] = useState<number | null>(null)
+    const [updateName, setUpdateName] = useState<string>("")
+    const [updateLat, setUpdateLat] = useState<number | null>(null)
+    const [updateLng, setUpdateLng] = useState<number | null>(null)
+
+    const startUpdate = (location: Location) => {
+      setUpdateId(location.id)
+      setUpdateName(location.name)
+      setUpdateLat(location.lat)
+      setUpdateLng(location.lng)
+    }
+
 
     async function fetchMapData() {
-      const res = await fetch("/api/map")
-      const data = await res.json()
-      setLocations(data)
+      try {
+        setLoading(true)
+        const res = await fetch("/api/map")
+        const data = await res.json()
+        setLocations(data)
+      } catch (error) {
+        console.error("Något gick fel", error)
+      } finally {
+        setLoading(false)
+      }
+  
     }
 
   useEffect(()=> {
       fetchMapData()
   }, [update])
 
-  useEffect(() => {
-    async function fetchLocations() {
-      try {
-          const res = await fetch("/api/map")
-          if (!res.ok) {
-          throw new Error("Failed to fetch data")
-          }
-        const data: Location[] = await res.json()
-        setLocations(data)
-      } catch {
-        setError("Kunde inte hämtas data från API")
-      } finally {
-        setLoading(false)
-      }
-    }
-    fetchLocations()
-  }, [])
-
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault()
     const res = await fetch("/api/map", {
       method: 'post',
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify({name, lat, lng})
     })
 
@@ -62,18 +66,54 @@ export default function PlacePage() {
     setName("")
     setLat(undefined)
     setLng(undefined)
-
     setUpdate(!update)
+  }
 
+  async function handleDelete(id: number) {
+    await fetch("/api/map", {
+      method: "delete",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({id})
+    })
+
+    const res = await fetch("/api/map")
+    const data = await res.json()
+    setLocations(data)
+    setUpdate(!update)
+  }
+
+  const handleUpdate = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    if (updateId === null || updateLat === null || updateLng === null) return
+
+    const updatedLocation = {
+      id: updateId,
+      name: updateName,
+      lat: updateLat,
+      lng: updateLng
+    }
+
+    const res = await fetch("/api/map", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(updatedLocation)
+    })
+
+    if (res.ok) {
+      setUpdateId(null)
+      setUpdate(!update)
+    }
   }
 
   if (loading) {
     return<p className="mt-10 ml-10 text-2xl">Laddar...</p>
   }
 
-  if (error) {
-    return <p>{error}</p>
-  }
 
   return (
 
@@ -86,10 +126,25 @@ export default function PlacePage() {
             <strong>{location.name}</strong>
             <br/>
             Lat: {location.lat}, Lng: {location.lng}
+            <button onClick={() => startUpdate(location)}
+            className="ml-5 p-1 border border-green-700 bg-green-400 hover:bg-green-200">Redigera</button>
+            {updateId === location.id && (
+              <form onSubmit={handleUpdate} className="mt-5 mb-5 bg-amber-200">
+                <input value={updateName} className="bg-amber-100 m-2"
+                onChange={e => setUpdateName(e.target.value)}/>
+                <input type="number" value={updateLat ?? ""} className="bg-amber-100 m-2"
+                onChange={e => setUpdateLat(Number(e.target.value))} />
+                <input type="number" value={updateLng ?? ""} className="bg-amber-100 m-2"
+                onChange={e => setUpdateLng(Number(e.target.value))} />
+                <button type="submit" className="bg-emerald-200 m-2 border border-emerald-600 hover:bg-emerald-100">Spara ändringar</button>
+              </form>
+            )}
+            <button onClick={()=> handleDelete(location.id)} className="ml-5 p-1 border border-red-500 bg-red-300 hover:bg-red-200">Ta bort</button>
+
           </li>
         ))}
       </ul>
-      
+
     <form onSubmit={handleSubmit}>
       <input className="bg-amber-50 mt-5 border border-amber-600 pl-2" onChange={(e)=>setName(e.target.value)} type="text" value={name} placeholder="Sriv en plats..." />
         <br/>
@@ -97,8 +152,9 @@ export default function PlacePage() {
         <br/>
       <input className="bg-amber-50 mt-5 border border-amber-600 pl-2" onChange={(e)=> setLng(Number(e.target.value))} value={lng ?? ""} type="number"/>
         <br/>
-      <button className="bg-amber-600 mt-5 w-20 border border-amber-900">Send</button>
+      <button className="bg-amber-600 mt-5 w-20 border border-amber-900 hover:bg-amber-500">Skicka</button>
       </form>
+
        
     </div>
 
